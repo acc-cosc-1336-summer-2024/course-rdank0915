3abb197da536d72adf4c14a3ba1524857c7f60e5
rdank0915 <rdank0915@gmail.com>
2024-06-23 01:05:42 +0000

    Homework 4

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -17,8 +17,78 @@ def sum_odd_numbers(num):
     val = 1
 
     while val <= num:
-        if val % 2 != 0:
-            sum += val
+        
+        sum = sum + val
+
+        val += 2
 
     return sum
 
+def display_menu():
+    print("Homework 3 Menu")
+    print("1-Factorial")
+    print("2-Sum Odd Numbers")
+    print("3-Exit")
+
+def run_menu():
+    option = "1"
+    while(option != "3"):
+        display_menu()
+        option = input("Enter your menu option: ")
+        handle_menu_option(option)
+
+def handle_menu_option(option):
+    if(option == '1'):
+        option_1()
+    elif(option == '2'):
+        option_2()
+    elif(option == '3'):
+        option_3()
+    else:
+        print("Invalid Option")
+
+def option_1():
+    num = int(input("Enter a number between 0 and 10: "))
+    if num > 0 and num < 10:
+        print(get_factorial(num))
+    else:
+        num = int(input("Number invalid..Please enter a number between 0 and 10: "))
+        if num > 0 and num < 10:
+            print(get_factorial(num))
+
+    choice = "Y"
+    while(choice == "Y" or choice == "N"):
+        choice = input("Would you like to exit Y or N: ")
+        if choice == "Y":
+            print(run_menu())
+        elif choice == "N":
+            print(option_1())
+
+def option_2():
+    num = int(input("Enter a number between 0 and 100: "))
+    if num > 0 and num < 100:
+        print(sum_odd_numbers(num))
+    else:
+        num = int(input("Number invalid..Please enter a number between 0 and 100: "))
+        if num > 0 and num < 100:
+            print(sum_odd_numbers(num))
+    
+    choice = "Y"
+    while(choice == "Y" or choice == "N"):
+        choice = input("Would you like to exit Y or N: ")
+        if choice == "Y":
+            print(run_menu())
+        elif choice == "N":
+            print(option_2())
+
+def option_3():
+    choice = input("Would you like to exit Y or N: ")
+    if choice == "Y":
+        print("Exiting... ")
+    elif choice == "N":
+        run_menu()
+    else:
+        print("Invalid Option")
+    
+    
+    
