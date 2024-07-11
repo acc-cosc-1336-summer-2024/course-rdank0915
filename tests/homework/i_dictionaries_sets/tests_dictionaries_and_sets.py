c7088251fc2950445b83649c7cf1a321fa3b5f03
rdank0915 <rdank0915@gmail.com>
2024-07-11 03:12:59 +0000

    Homework 6

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -11,9 +11,9 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(0.4, get_p_distance(['T','T','T','C','C','A','T','T','T','A'], ['G','A','T','T','C','A','T','T','T','C']))
 
     def test_get_p_distance_matrix1(self):
-        #Test that get_p_distance_matrix with parameter value ['T','T','T','C','C','A','T','T','T','A'],
-        #['G','A','T','T','C','A','T','T','T','C'], ['T','T','T','C','C','A','T','T','T','T'], and
-        #['G','T','T','C','C','A','T','T','T','A'] returns the matrix [0.0, 0.4, 0.1, 0.1], [0.4, 0.0, 0.4, 0.3],
+        #Test that get_p_distance_matrix with parameter value [TTTCCATTTA],
+        #[GATTCATTTC], [TTTCCATTTT], and
+        #[GTTCCATTTA] returns the matrix [0.0, 0.4, 0.1, 0.1], [0.4, 0.0, 0.4, 0.3],
         #[0.1, 0.4, 0.0, 0.2], [0.1, 0.3, 0.2, 0.0]
         taxa_list = [['T','T','T','C','C','A','T','T','T','A'], ['G','A','T','T','C','A','T','T','T','C'], ['T','T','T','C','C','A','T','T','T','T'], ['G','T','T','C','C','A','T','T','T','A']]
         self.assertEqual([[0.0, 0.4, 0.1, 0.1], [0.4, 0.0, 0.4, 0.3], [0.1, 0.4, 0.0, 0.2], [0.1, 0.3, 0.2, 0.0]], get_p_distance_matrix(taxa_list))
