c4197d145d01627aa298dde83da8d91d07ef06c6
rdank0915 <rdank0915@gmail.com>
2024-07-14 00:02:47 +0000

    Homework 5

--- tests/homework/h_strings/tests_strings.py
+++ tests/homework/h_strings/tests_strings.py
@@ -6,9 +6,10 @@ from src.homework.h_strings.strings import get_dna_complement
 class Test_Config(unittest.TestCase):
 
     def test_get_hamming_distance1(self):
-        #Test that get_hamming_distance with parameter value 4 returns the value 24
+        #Test that get_hamming_distance with parameter value GAGCCTACTAACGGGAT and
+        #CATCGTAATGACGGCCT that returns 7
         self.assertEqual(7, get_hamming_distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"))
 
     def test_get_dna_complement1(self):
-        #Test that sum_odd_numbers with parameter value 7 returns 16
+        #Test that get_dna_complement with parameter value AAAACCCGGT returns ACCGGGTTTT
         self.assertEqual("ACCGGGTTTT", get_dna_complement("AAAACCCGGT"))
