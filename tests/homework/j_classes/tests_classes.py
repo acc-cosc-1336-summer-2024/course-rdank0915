17b004a196d84371e6fa7a9136fca1204e87f506
rdank0915 <rdank0915@gmail.com>
2024-07-18 01:04:20 +0000

    Homework 7

--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -6,5 +6,7 @@ class Test_Config(unittest.TestCase):
 
     #Write a test to make sure that get_rolled_value returns a value from 1 to 6
     def test_get_rolled_value1(self):
-        roll = Die()
-        self.assertEqual(Die.get_rolled_value())
+        die = Die()
+        for _ in range(3):
+            die.roll()
+            print(die.get_rolled_value)
